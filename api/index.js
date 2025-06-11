9b2acced1f8c2883eafa78131f1b6d8982de4031
EsmailKhaleel <esmailkhaleel27@gmail.com>
2025-06-11 09:03:27 +0300

    fix: update file path handling in readDB and writeDB functions

--- api/index.js
+++ api/index.js
@@ -13,13 +13,13 @@ server.use(express.json());
 
 // Helper function to read the database file
 const readDB = () => {
-  const data = fs.readFileSync('../db.json', 'utf8');
+  const data = fs.readFileSync(require('path').join(__dirname, '../db.json'), 'utf8');
   return JSON.parse(data);
 };
 
 // Helper function to write to the database file
 const writeDB = (data) => {
-  fs.writeFileSync('../db.json', JSON.stringify(data, null, 2));
+  fs.writeFileSync(require('path').join(__dirname, '../db.json'), JSON.stringify(data, null, 2));
 };
 
 // Products Routes
