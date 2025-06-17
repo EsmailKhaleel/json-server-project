d302188211ff98269a51abbbb23a09c27e012b81
EsmailKhaleel <esmailkhaleel27@gmail.com>
2025-06-17 12:31:56 +0300

    fix: correct path for serving the welcome page HTML file

--- api/index.js
+++ api/index.js
@@ -45,7 +45,7 @@ app.use(express.static('public'));
 
 // Welcome page at root endpoint
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: './public' });
+  res.sendFile('../public/index.html', { root: './public' });
 });
 
 
